7f72d54bf41f34f4d9521cb1bfdc711217ea41c7
Trần Quang Huy <127572935+HuiiTran@users.noreply.github.com>
2025-02-28 17:48:15 +0700

    Completed 8

---
+++ test/BookStore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Authors/EfCoreAuthorAppService_Tests.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Authors;
+using BookStore.Authors;
+using BookStore.EntityFrameworkCore;
+using BookStore;
+using Xunit;
+
+namespace BookStore.EntityFrameworkCore.Applications.Authors;
+
+[Collection(BookStoreTestConsts.CollectionDefinitionName)]
+public class EfCoreAuthorAppService_Tests : AuthorAppService_Tests<BookStoreEntityFrameworkCoreTestModule>
+{
+
+}
